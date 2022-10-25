9d8453e22794a0aa27e59f5e1688d683c82bf676
Andrea-PaolaMartinez-Lopez <78820061+Andrea-Paola-Martinez-Lopez@users.noreply.github.com>
2022-10-25 12:32:27 -0500

    Update AcercaDe.js

--- src/Componentes/AcercaDe.js
+++ src/Componentes/AcercaDe.js
@@ -1,6 +1,6 @@
 import React, { Component }  from 'react';
 import imagenItsur from "../files/itsur.png"
-
+import logo from "../files/logo_chatbot.png"
 
 
 import '../css/style.css'
@@ -63,6 +63,14 @@ export const AcercaDe = () => {
                                     <h2>Giovanni de Jesus Alejo Calderon</h2>
                                     <p class="title">Estudiante de Ing. Sistemas Computacionales</p>
                                     <p>s19120147@alumnos.itsur.edu.mx</p>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="column">
+                            <div className="card">
+                                <div className="container" >
+                                  <img src={logo} width="200px" height="200px"></img>
+
                                 </div>
                             </div>
                         </div>
